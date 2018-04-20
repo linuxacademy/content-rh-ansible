e2dc36fdfdb115959db4b92bccc1329e0e37d72a
Stosh Olham <stosh@linuxacademy.com>
2018-04-20 15:41:15 -0400

    Added comments.  Added line to remove transaction_list file on startup.

--- lab5/control/scripts/change_l33t.sh
+++ lab5/control/scripts/change_l33t.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# Is httpd running?
 nc -z -w 50ms apps.l33t.com 80 > /dev/null
 
+
+# Switch states depending on if it is up or down
 if [ $? -ne 0 ];
 then
   echo -n "Starting apps.l33t.com...";
+  # Remove transaction_list, otherwise playbook will not check url again.
+  rm -f /home/ansible/transaction_list > /dev/null;
   ansible node1 -b -m service -a "name=httpd state=started" > /dev/null;
   echo "done."
 else
