e6456d17ff274d7526d81407782fea9bde6e7e77
Stosh Olham <stosh@linuxacademy.com>
2018-06-14 15:29:12 -0400

    Minor fixes to ad-hoc.sh, security.yml.  Added demo file and ad-hoc2.sh.

---
+++ lab9/exam/ad-hoc-2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+## Write Ansible ad-hoc commands below
+
+# Create new file called /opt/test.sh
+
+# Set owner of the file to ansible
+
+# Set mode to 0755
+
+# Add content to the file:
+# #!/bin/sh
+# echo hi there
