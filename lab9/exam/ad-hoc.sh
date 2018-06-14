05f6f8917d818293e4d2ccaa21288f10e454e8f1
Stosh Olham <stosh@linuxacademy.com>
2018-06-14 13:32:39 -0400

    Initial commit for lab 9 artifacts.

---
+++ lab9/exam/ad-hoc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+## This script should run without error completing the documented tasks before completing the exam.
+
+# Create the user john on labservers
+ansible labservers -m user -a "name=john"
+
+# Copy the file demo to /home/ansible/demo on node1
+ansible node1 copy -a "source=/home/ansible/files/demo dest=/home/ansible/demo"
+
+# Install elinks on labservers
+ansible -m yum -a "name=elinks state=latest"
---
+++ lab9/exam_inv/inv.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+import os
+import sys
+import argparse
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+class ExampleInventory(object):
+
+    def __init__(self):
+        self.inventory = {}
+        self.read_cli_args()
+
+        # Called with `--list`.
+        if self.args.list:
+            self.inventory = self.example_inventory()
+        # Called with `--host [hostname]`.
+        elif self.args.host:
+            # Not implemented, since we return _meta info `--list`.
+            self.inventory = self.empty_inventory()
+        # If no groups or vars are present, return empty inventory.
+        else:
+            self.inventory = self.empty_inventory()
+
+        print json.dumps(self.inventory);
+
+    # Example inventory for testing.
+    def example_inventory(self):
+        return {
+            'group': {
+                'hosts': ['node1', 'node2'],
+                'vars': {
+                    'example_variable': 'value'
+                }
+            },
+            '_meta': {
+                'hostvars': {
+                    'node1': {
+                        'host_specific_var': 'foo'
+                    },
+                    'node2': {
+                        'host_specific_var': 'bar'
+                    }
+                }
+            }
+        }
+
+    # Empty inventory for testing.
+    def empty_inventory(self):
+        return {'_meta': {'hostvars': {}}}
+
+    # Read the command line args passed to the script.
+    def read_cli_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action = 'store_true')
+        parser.add_argument('--host', action = 'store')
+        self.args = parser.parse_args()
+
+# Get the inventory.
+ExampleInventory()
