45cac92bd119584c4244b423d0509f8d38016450
Stosh Olham <stosh@linuxacademy.com>
2018-06-21 13:03:35 -0400

    Typo fix in ad-hoc.sh, added solution dir and moved soln files as needed

--- lab9/exam/ad-hoc.sh
+++ lab9/exam/ad-hoc.sh
@@ -5,7 +5,7 @@
 ansible labservers -m user -a "name=john"
 
 # Copy the file demo to /home/ansible/demo on node1
-ansible node1 copy -a "source=/home/ansible/files/demo dest=/home/ansible/demo"
+ansible node1 copy -a "src=/home/ansible/files/demo dest=/home/ansible/demo"
 
 # Install elinks on all hosts in the labservers host group
 ansible -m yum -a "name=elinks state=latest"
